0752deb2750543cae2c08e1ee4cb2dfee482134d
LuisPastorMartinez <lfpmda@gmail.com>
2025-09-09 15:19:31 +0100

    Add files via upload

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,7 @@
-# utils/__init__.py
 from .data_utils import limpiar_telefono, limpiar_fecha, get_next_id
 from .excel_utils import load_dataframes_local, save_dataframe_local
 from .firestore_utils import (
-    load_dataframes_firestore, 
+    load_dataframes_firestore,
     save_dataframe_firestore,
     delete_document_firestore
 )
