d195c328c1511e65523d8a7dbe40d6ab372c3b3b
LuisPastorMartinez <lfpmda@gmail.com>
2025-09-09 18:04:46 +0100

    Add files via upload

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .data_utils import limpiar_telefono, limpiar_fecha, get_next_id
+from .data_utils import limpiar_telefono, limpiar_fecha
 from .excel_utils import load_dataframes_local, save_dataframe_local
 from .firestore_utils import load_dataframes_firestore, save_dataframe_firestore, delete_document_firestore
 from .helpers import convert_to_firestore_type, safe_select_index
@@ -6,7 +6,6 @@ from .helpers import convert_to_firestore_type, safe_select_index
 __all__ = [
     'limpiar_telefono',
     'limpiar_fecha',
-    'get_next_id',
     'load_dataframes_local',
     'save_dataframe_local',
     'load_dataframes_firestore',
