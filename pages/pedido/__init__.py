2910a6a2cc19e6315209698d64cbcc47149cda8d
LuisPastorMartinez <lfpmda@gmail.com>
2025-09-11 21:23:35 +0100

    Delete pages directory

--- pages/pedido/__init__.py
+++
@@ -1,7 +0,0 @@
-# pages/pedido/__init__.py
-from .crear_pedido import show_create
-from .consultar_pedidos import show_consult
-from .modificar_pedido import show_modify
-from .eliminar_pedido import show_delete
-
-__all__ = ["show_create", "show_consult", "show_modify", "show_delete"]
