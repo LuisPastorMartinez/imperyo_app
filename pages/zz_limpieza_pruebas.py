31e4d7c6fe8eeab94bc16c203fe7b3734e03cccf
Luis Pastor Martinez <lfpmda@gmail.com>
2026-01-09 23:54:27 +0000

    empezar de 0

--- pages/zz_limpieza_pruebas.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from utils.cleanup_pedidos_duplicados import limpiar_pedidos_duplicados
-
-st.set_page_config(page_title="Limpieza pruebas", layout="wide")
-
-limpiar_pedidos_duplicados()
